6be6fb25e59b0bc1257dfe64a96d0dc0931b19b3
escgTobiW <146823902+escgTobiW@users.noreply.github.com>
2025-05-23 08:47:04 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Scripts/findingScript.cs
+++ Assets/Scripts/findingScript.cs
@@ -6,28 +6,51 @@ public class findingScript : MonoBehaviour
     public bool nearPlayer = false;
     //public int totalTeru = 10;
 
+    public GameObject Confetti;
 
-
+    public float wait = 0;
+    bool waitTime = false;
 
     void Start()
     {
-        
+        Confetti.SetActive(false);
     }
 
 
     void Update()
     {
 
+
+        if (waitTime == true)
+        {
+            wait += Time.deltaTime;
+        }
+
+        if (waitTime == false)
+        {
+            wait = 0;
+        }
+
         if ((Input.GetMouseButtonDown(1) == true) && (nearPlayer == true))
         {
 
+            //make count go down
             countText.instance.SetCount(1);
 
-            //make count go down
-            //totalTeru--;
-            //countText.totalTeru--;
 
-            //make screen text say well done or something
+            Confetti.SetActive(true);
+            
+  
+
+            waitTime = true;
+
+            
+        }
+
+        if (wait > 0.7f)
+        {
+            
+            waitTime = false;
 
             //make object go away
             Destroy(gameObject);
